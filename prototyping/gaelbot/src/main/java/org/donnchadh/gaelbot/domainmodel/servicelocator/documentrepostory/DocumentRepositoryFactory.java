498edb9190f5d20ba95beca09c887323fcf3e59c
Donnchadh Ó Donnabháin <donnchadh@gmail.com>
2008-08-07 16:31:11 +0000

    refactoring

--- prototyping/gaelbot/src/main/java/org/donnchadh/gaelbot/domainmodel/servicelocator/documentrepostory/DocumentRepositoryFactory.java
+++
@@ -1,9 +0,0 @@
-package org.donnchadh.gaelbot.domainmodel.servicelocator.documentrepostory;
-
-import org.donnchadh.gaelbot.domainmodel.DocumentRepository;
-
-public interface DocumentRepositoryFactory {
-    DocumentRepository getDocumentRepository(String path);
-
-    DocumentRepository getDefaultDocumentRepository();
-}
